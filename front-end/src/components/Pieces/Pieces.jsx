f1c8bbd627576e74f73ae2d280421ed13e5c6837
Peter Hajdu <hajdu2024@gmail.com>
2024-10-09 19:52:13 +0200

    update

--- front-end/src/components/Pieces/Pieces.jsx
+++ front-end/src/components/Pieces/Pieces.jsx
@@ -1,45 +1,20 @@
 import "./Pieces.css";
-import Piece from "./Piece.jsx"
+import Piece from "./Piece.jsx";
+import {useState} from 'react';
+import { createPosition } from "../../helper.js";
 
 function Pieces() {
-  const position = new Array(8).fill('').map(x => new Array(8).fill(''));
-
-  for (let i = 0; i< 8; i++){
-    position[1][i]= "wp"
-    position[6][i]="bp"
-  }
-
-
-
-  position[0][0] = "wr"
-  position[0][1] = "wn"
-  position[0][2] = "wb"
-  position[0][3] = "wq"
-  position[0][4] = "wk"
-  position[0][5] = "wb"
-  position[0][6] = "wn"
-  position[0][7] = "wr"
-
-  position[7][0] = "br"
-  position[7][1] = "bn"
-  position[7][2] = "bb"
-  position[7][3] = "bq"
-  position[7][4] = "bk"
-  position[7][5] = "bb"
-  position[7][6] = "bn"
-  position[7][7] = "br"
-
-  console.log(position);
+  const [state, setState] = useState(createPosition());
 
   return<div className="pieces">
-          {position.map((r,rank) =>
+          {state.map((r,rank) =>
             r.map((f,file) =>
-              position[rank][file]
+              state[rank][file]
               ?     <Piece  
                       key={rank+"-"+file}
                       file={file}
                       rank={rank}
-                      piece={position[rank][file]}
+                      piece={state[rank][file]}
                   />
               :   null
           ))}
