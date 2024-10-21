b22d9ec98a11048aff6396e4c64883ff6b492c0c
Peter Hajdu <hajdu2024@gmail.com>
2024-10-21 17:48:35 +0200

    Moving

--- front-end/src/components/Pieces/Pieces.jsx
+++ front-end/src/components/Pieces/Pieces.jsx
@@ -1,16 +1,45 @@
 import "./Pieces.css";
 import Piece from "./Piece.jsx";
-import {useState} from 'react';
-import { createPosition } from "../../helper.js";
+import {useState, useRef} from 'react';
+
+import { createPosition, copyPosition } from "../../helper.js";
 
 function Pieces() {
+  const ref = useRef();
   const [state, setState] = useState(createPosition());
 
-  return<div className="pieces">
+  const calculatePosition = e => {
+    const {width,left,top} = ref.current.getBoundingClientRect()
+    const size = width /8;
+    const y =  Math.floor((e.clientX-left) / size);
+    const x = 7 - Math.floor((e.clientY-top) / size);
+    return {x,y}
+  }
+
+  const onDrop = e => {
+    const newPosition = copyPosition(state)
+    const {x,y} = calculatePosition(e)
+    const [p,file,rank] = e.dataTransfer.getData('text').split(",")
+    
+    
+    newPosition[rank][file] = ''
+    newPosition[x][y] = p
+    
+    setState(newPosition)
+  }
+  const onDragOver = e => {
+    e.preventDefault();
+  }
+
+  return<div 
+      ref = {ref}
+      onDrop={onDrop}
+      onDragOver={onDragOver}
+      className="pieces">
           {state.map((r,rank) =>
             r.map((f,file) =>
               state[rank][file]
-              ?     <Piece  
+              ?     <Piece 
                       key={rank+"-"+file}
                       file={file}
                       rank={rank}
